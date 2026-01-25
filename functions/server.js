51966bcde54df273c9a0aab7f97535a95634f9ad
SimaThapa <sima.thapa3333@gmail.com>
2026-01-25 09:41:38 +0545

    esewa integareted

--- functions/server.js
+++ functions/server.js
@@ -6,8 +6,12 @@ const cors = require("cors");
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/api/payment", require("./routes/orderPaymentRoutes"));
