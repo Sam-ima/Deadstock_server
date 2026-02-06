cb23582ae61d8c52e00174a45c46489e4e821e8f
Roshni Giri <roshanigiri82@gmail.com>
2026-02-06 22:40:39 +0545

    updated at rule

--- functions/pricing/rules/furnitureRule.js
+++ functions/pricing/rules/furnitureRule.js
@@ -1,4 +1,7 @@
+// pricing/rules/furnitureRule.js
+
 module.exports.furnitureRule = (product, ageDays) => {
-  const yearly = Math.min((ageDays / 365) * 0.10, 0.40);
-  return product.original_price * (1 - yearly);
+  const rate = -0.0008; // slow depreciation 0.08% daily
+  const price = Number(product.currentPrice || product.basePrice);
+  return price * Math.pow(1 + rate, ageDays);
 };
